568c958e6ec9552bf30a5c496ef4c5b79b599d55
tpena <tito.pena@gmail.com>
2013-09-26 00:50:16 +0200

    correct structure for egginfo

---
+++ smarkets/seto/__init__.py
@@ -0,0 +1,5 @@
+"Protocol-buffers generated package"
+# Copyright (C) 2011 Smarkets Limited <support@smarkets.com>
+#
+# This module is released under the MIT License:
+# http://www.opensource.org/licenses/mit-license.php
